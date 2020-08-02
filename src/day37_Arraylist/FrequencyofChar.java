2ed79b25854d06fc07bf73be3531b0bcbdaf5ecd
afsammar <afsammar96@gmail.com>
2020-08-02 10:59:32 -0400

    Frequency of chars

---
+++ src/day37_Arraylist/FrequencyofChar.java
@@ -0,0 +1,23 @@
+package day37_Arraylist;
+
+import library.Util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FrequencyofChar {
+    public static void main(String[] args) {
+
+        String str = "AAABBC";
+        ArrayList<String> list = new ArrayList<>();
+        for (String each : str.split("")) {
+            list.add(each);
+        }
+        String nonDup = Util.removedup(str);
+        for (String each : nonDup.split("")) {
+            int count = Collections.frequency(list, each);
+            System.out.print(each + count);
+        }
+
+    }
+}
