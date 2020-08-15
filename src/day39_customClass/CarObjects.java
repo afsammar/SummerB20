c445a054ae0948ec964c8dddf5b1b7eb84b5d156
afsammar <afsammar96@gmail.com>
2020-08-15 16:39:26 -0400

    customClassCar

--- src/day39_customClass/CarObjects.java
+++ src/day39_customClass/CarObjects.java
@@ -2,33 +2,50 @@ package day39_customClass;
 
 public class CarObjects {
     public static void main(String[] args) {
-
         car car1 = new car();
+       /*
         car1.brand = "Toyota";
         car1.model = "Corolla";
         car1.year = 2020;
-        car1.color ="White";
+        car1.color = "White";
         car1.milage = 20000;
-        car1.price = 19000.50;
+        car1.price = 19000.50;*/
+        car1.setInfo("Toyota","Corolla",2020,"Red",20000,55000);
+
 
         System.out.println(car1.brand);
         System.out.println(car1.model);
         System.out.println(car1.year);
         System.out.println(car1.color);
-
-
+        System.out.println();
+        System.out.println("===========================");
+        System.out.println();
         car car2 = new car();
-
-        car2.brand = "Mercedes";
+        car2.setInfo("BMW","X5",2020,"Black",55550,1000);
+       /* car2.brand = "Mercedes";
         car2.model = "G 550";
         car2.year = 2000;
-        car2.color ="Blue";
+        car2.color = "Blue";*/
 
         System.out.println(car2.brand);
         System.out.println(car2.model);
         System.out.println(car2.year);
         System.out.println(car2.color);
+        System.out.println("===========================");
 
 
+        car car3 = new car();
+        car3.setInfo("Audi","A7",2020,"White",32000,3500);
+        car3.getInfo();
+
+
+        car car4=new car();
+        car4.setInfo("Jeep","Wrangler",1990,"Blue",17000,25000);
+
+        car4.getInfo();
     }
+
+
+
+
 }
--- src/day39_customClass/car.java
+++ src/day39_customClass/car.java
@@ -15,4 +15,20 @@ public class car {
         double milage;
         double price;
 
+
+        public void setInfo(String carBrand, String carModel, int carYear, String carColor, double carPrice, double carmileage ){
+                brand = carBrand;
+                model = carModel;
+                year = carYear;
+                color = carColor;
+                price = carPrice;
+                milage = carmileage;}
+
+        public void getInfo(){
+                System.out.println(year + " "+brand+" "+model+", "+color+", "+milage+", $"+price);
+        }
+        public void start(){
+                System.out.println(brand+" "+model+" is started");
+        }
+
 }
