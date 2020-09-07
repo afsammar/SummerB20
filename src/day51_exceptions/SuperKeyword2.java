7551b59c679a18195af149f67c0d9e77cd94c707
afsammar <afsammar96@gmail.com>
2020-09-07 12:09:27 -0400

    codes

--- src/day51_exceptions/SuperKeyword2.java
+++ src/day51_exceptions/SuperKeyword2.java
@@ -1,4 +1,18 @@
 package day51_exceptions;
+class B{
 
-public class SuperKeyword2 {
+    public B(){
+        System.out.println("Super class default constructor");
+    }
+}
+
+public class SuperKeyword2 extends B {
+
+    public SuperKeyword2(){
+        System.out.println("Sub class default constructor");
+    }
+
+    public static void main(String[] args) {
+        new SuperKeyword2();
+    }
 }
